aefe3d54b10ce31d723f111d00d395c0a676b686
Pham Quang Minh <pminh12012005@gmail.com>
2025-10-16 10:33:29 +0700

    Add admin account

--- my-react-app/server/backend/main.py
+++ my-react-app/server/backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from db.connection import db, users_collection
+from core.security import hash_password
 from routes import auth
 
 app = FastAPI()
@@ -13,6 +15,25 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
+
+@app.on_event("startup")
+async def startup_event():
+
+    # Check if admin exists
+    admin_email = "nothingnoteworthy@gmail.com"
+    existing_admin = await users_collection.find_one({"email": admin_email})
+
+    if not existing_admin:
+
+        admin_user = {
+            "firstName": "Admin",
+            "lastName": "Sir",
+            "email": admin_email,
+            "password": hash_password("processor123"),
+            "role": "admin",
+        }
+
+        await users_collection.insert_one(admin_user)
 
 @app.get("/")
 def home():
